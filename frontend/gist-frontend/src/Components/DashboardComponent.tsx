c519da5222fbcdeb7c43611cf760d32909ca8bab
katebrune <kbrune@iastate.edu>
2019-10-23 00:33:29 -0500

    added left navigation with slide out for groups

--- frontend/gist-frontend/src/Components/DashboardComponent.tsx
+++ frontend/gist-frontend/src/Components/DashboardComponent.tsx
@@ -1,53 +1,123 @@
 import React, { Component } from "react";
-import { Grommet, Grid, Box, Button, Text } from "grommet";
+import { Box, Button, Text, Grommet } from "grommet";
 import { User, Group } from "grommet-icons";
+import { Row } from "antd";
 import { theme } from "../Helpers/theme";
 
 export default class DashboardComponent extends Component {
-  render() {
+  state = {
+    isGroupOpen: false
+  };
+
+  toggleGroupSidebar = () => {
+    const { isGroupOpen } = this.state;
+    this.setState({
+      isGroupOpen: !isGroupOpen
+    });
+  };
+
+  getOpenGroupsComponent = () => {
     return (
-      <>
-        <Grommet full theme={theme}>
-          <Grid
-            fill
-            rows={["auto", "flex"]}
-            columns={["auto", "flex"]}
-            areas={[
-              { name: "sidebar", start: [0, 1], end: [0, 1] },
-              { name: "main", start: [1, 1], end: [1, 1] }
-            ]}
+      <Row
+        type="flex"
+        justify="center"
+        align="top"
+        style={{ gridTemplateColumns: "4vw 11vw 85vw", display: "grid" }}
+      >
+        <div
+          style={{
+            height: "100vh",
+            backgroundColor: "#2AF598",
+            width: 82,
+            boxShadow: "10px 10px 5px grey"
+          }}
+        >
+          <Button key={"user"} href="#" hoverIndicator>
+            <Box
+              pad={{ horizontal: "medium", vertical: "small" }}
+              style={{ paddingLeft: 28 }}
+            >
+              <User color="light-2" />
+            </Box>
+          </Button>
+          <Button
+            key={"group"}
+            href="#"
+            hoverIndicator
+            onClick={this.toggleGroupSidebar}
           >
             <Box
-              gridArea="sidebar"
-              background={{ color: "#2AF598" }}
-              width="xsmall"
-              animation={[
-                { type: "fadeIn", duration: 300 },
-                { type: "slideRight", size: "xlarge", duration: 150 }
-              ]}
+              pad={{ horizontal: "medium", vertical: "small" }}
+              style={{ paddingLeft: 28 }}
             >
-              <Button key={"user"} href="#" hoverIndicator>
-                <Box
-                  pad={{ horizontal: "medium", vertical: "small" }}
-                  style={{ paddingLeft: 35 }}
-                >
-                  <User color="light-2" />
-                </Box>
-              </Button>
-              <Button key={"group"} href="#" hoverIndicator>
-                <Box
-                  pad={{ horizontal: "medium", vertical: "small" }}
-                  style={{ paddingLeft: 35 }}
-                >
-                  <Group color="light-2" />
-                </Box>
-              </Button>
+              <Group color="light-2" />
             </Box>
+          </Button>
+        </div>
+        <div
+          style={{
+            height: "100vh",
+            backgroundColor: "#2AF598",
+            opacity: 0.9,
+            width: 212
+          }}
+        ></div>
 
-            <Box gridArea="main" justify="center" align="center">
-              <Text>main</Text>
+        <div>Main Content</div>
+      </Row>
+    );
+  };
+
+  getClosedGroupsComponent = () => {
+    return (
+      <Row
+        type="flex"
+        justify="center"
+        align="top"
+        style={{ gridTemplateColumns: "4vw  96vw", display: "grid" }}
+      >
+        <div
+          style={{
+            height: "100vh",
+            backgroundColor: "#2AF598",
+            width: 78
+          }}
+        >
+          <Button key={"user"} href="#" hoverIndicator>
+            <Box
+              pad={{ horizontal: "medium", vertical: "small" }}
+              style={{ paddingLeft: 28 }}
+            >
+              <User color="light-2" />
             </Box>
-          </Grid>
+          </Button>
+          <Button
+            key={"group"}
+            href="#"
+            hoverIndicator
+            onClick={this.toggleGroupSidebar}
+          >
+            <Box
+              pad={{ horizontal: "medium", vertical: "small" }}
+              style={{ paddingLeft: 28 }}
+            >
+              <Group color="light-2" />
+            </Box>
+          </Button>
+        </div>
+
+        <div>Main Content</div>
+      </Row>
+    );
+  };
+  render() {
+    const { isGroupOpen } = this.state;
+    return (
+      <>
+        <Grommet theme={theme}>
+          {isGroupOpen
+            ? this.getOpenGroupsComponent()
+            : this.getClosedGroupsComponent()}
         </Grommet>
       </>
     );
